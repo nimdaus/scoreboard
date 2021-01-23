789e5df6a07c7111a7b4b2c11afb4dc8c39ebb56
nimdaus <30591465+nimdaus@users.noreply.github.com>
2021-01-23 13:15:30 -0500

    Update update.py

--- update.py
+++ update.py
@@ -8,21 +8,22 @@ try:
 except Exception:
 	pass
 
-print("Refreshing from source")
+print("Refreshing from source and restarting")
 try:
 	check_call(["pkill", "-9", "-f", "webui"])
 except Exception:
 	pass
 
 proc = subprocess.Popen('screen -S webui', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-proc.stdin.write('git reset --hard\n')
-proc.stdin.write('git checkout main\n')
-proc.stdin.write('git pull\n')
-proc.stdin.write('source webui/bin/activate\n')
-proc.stdin.write('flask run\n')
+proc.stdin.write(git reset --hard + '\n')
+proc.stdin.write(git checkout main + '\n')
+proc.stdin.write(git pull + '\n')
+proc.stdin.write(source webui/bin/activate + '\n')
+proc.stdin.write(flask run + '\n')
 statusProc = subprocess.run('screen -ls', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-if statusProc.stdout == 0:
+print(statusProc.stdout)
+'''if statusProc.stdout == 0:
 	print("Success")
 	time.sleep(2)
 	subprocess.run("exit()")
