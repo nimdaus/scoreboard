817ac4a520c0ee778ebd9fafec726c0705665e46
nimdaus <30591465+nimdaus@users.noreply.github.com>
2021-01-23 11:38:21 -0500

    Update update.py

--- update.py
+++ update.py
@@ -5,13 +5,13 @@ print("Killing WebUI")
 try:
 	check_call(["pkill", "-9", "-f", "flask"])
 except:
-continue
+	continue
 
 print("Refreshing from source")
 try:
 	check_call(["pkill", "-9", "-f", "webui"])
 except:
-continue
+	continue
 
 proc = subprocess.Popen('screen -S webui', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 proc.stdin.write('cd ~/scoreboard\n')
