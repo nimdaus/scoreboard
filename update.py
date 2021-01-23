c0659da7c6f4ad6d46007f3097abd3d597691704
nimdaus <30591465+nimdaus@users.noreply.github.com>
2021-01-23 11:30:24 -0500

    Add files via upload

---
+++ update.py
@@ -0,0 +1,25 @@
+from subprocess import check_call, Popen
+import time
+
+print("Killing WebUI")
+check_call(["pkill", "-9", "-f", flask])
+print("Refreshing from source")
+check_call(["pkill", "-9", "-f", webui])
+proc = subprocess.Popen('screen -S webui', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+proc.stdin.write('cd ~/scoreboard\n')
+proc.stdin.write('git reset --hard\n')
+proc.stdin.write('git checkout main\n')
+proc.stdin.write('git pull\n')
+proc.stdin.write('source webui/bin/activate\n')
+proc.stdin.write('flask run\n')
+statusProc = subprocess.run('screen -ls', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+if statusProc.stdout == 0:
+	print("Success")
+	time.sleep(2)
+	subprocess.run("exit()")
+else:
+	print("Something went wrong")
+	time.sleep(2)
+	subprocess.run("exit()")
+# parse screen's output (statusString) for your status list
