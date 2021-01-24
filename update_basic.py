15c38e3b9cb0a89a61b2e7c41be2d6216759b54d
nimdaus <30591465+nimdaus@users.noreply.github.com>
2021-01-23 21:37:47 -0500

    Add files via upload

---
+++ update_basic.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.run(["deactivate", shell=True])
+subprocess.run(["git reset --hard", shell=True])
+subprocess.run(["git checkout main", shell=True])
+subprocess.run(["git pull", shell=True])
+subprocess.run(["source webui/bin/activate", shell=True])
+subprocess.run(["flask run", shell=True])
