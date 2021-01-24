41fdf672eb4fbb1ed7a078fc971d95b5b471ef64
nimdaus <30591465+nimdaus@users.noreply.github.com>
2021-01-23 21:44:40 -0500

    Update update_basic.py

--- update_basic.py
+++ update_basic.py
@@ -1,8 +1,8 @@
 import subprocess
 
-subprocess.run(["deactivate", shell=True])
-subprocess.run(["git reset --hard", shell=True])
-subprocess.run(["git checkout main", shell=True])
-subprocess.run(["git pull", shell=True])
-subprocess.run(["source webui/bin/activate", shell=True])
-subprocess.run(["flask run", shell=True])
+subprocess.run("deactivate", shell=True)
+subprocess.run(["git reset", "--hard"], shell=True)
+subprocess.run(["git checkout", "main"], shell=True)
+subprocess.run("git pull", shell=True)
+subprocess.run(["source", "webui/bin/activate"], shell=True)
+subprocess.run("flask run", shell=True)
